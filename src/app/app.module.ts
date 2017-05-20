735dff10583064ad8f1c2404fc0350a089d7cb44
Julien Teyteau <iuliust@gmail.com>
2017-05-20 23:32:18 +0200

    feat(styles) import MaterialModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'jsr-slimfast'}),
+    MaterialModule,
     CommonModule,
     HttpModule,
     AppRoutingModule
